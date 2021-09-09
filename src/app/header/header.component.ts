fea5a11777988b1c9e7f11c369c4415c6189a54e
akyeshi <akyeshi@gmail.com>
2021-09-09 10:01:56 -0400

    added html content to header component & referenced it in app.component.html file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  missionName: string = "Mars 2030"; 
+  rocketName: string = "Plasma Max"; 
+
   constructor() { }
 
   ngOnInit() {
