0da25ea7d86816273bc8ad6cfc19aa4e4fdee428
akyeshi <akyeshi@gmail.com>
2021-09-09 10:36:59 -0400

    experiment component created, in it html added, component referred in app.component.html file, css added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
 import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentComponent } from './experiment/experiment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CrewComponent,
-    EquipmentComponent
+    EquipmentComponent,
+    ExperimentComponent
   ],
   imports: [
     BrowserModule
